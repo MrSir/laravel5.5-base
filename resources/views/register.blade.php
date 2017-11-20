ecd8041e1eea3c1c42371b704aa6b1e6516843b9
Mitko Tochev <mr.sir88@gmail.com>
2017-11-19 20:22:03 -0500

    setup the register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,31 +2,62 @@
 
 @section('content')
     <br/>
+    <div class="alert hide" role="alert" id="alert"></div>
     <div class="card">
         <h4 class="card-header">Register</h4>
         <div class="card-body">
-            <form method="POST" action="/api/user">
+            <div id="register">
                 <div class="form-group">
                     <label for="name">Name</label>
                     <input type="text" class="form-control" id="name" placeholder="e.g. John Smith">
                 </div>
                 <div class="form-group">
-                    <label for="name">Email</label>
+                    <label for="email">Email</label>
                     <input type="email" class="form-control" id="email" placeholder="e.g. jsmith@gmail.com">
                 </div>
                 <div class="form-group">
-                    <label for="name">Password</label>
-                    <input type="password" class="form-control" id="password" name="password-confirmation">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
                 </div>
                 <div class="form-group">
-                    <label for="name">Confirm Password</label>
-                    <input type="password" class="form-control" id="password-confirmation" name="password-confirmation">
+                    <label for="password_confirmation">Confirm Password</label>
+                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+                <button type="button" class="btn btn-primary" onclick="javascript: register()">Submit</button>
+            </div>
             <hr/>
             <a href="/redirect/google" class="btn btn-secondary">Register With Google</a>
             <a href="/redirect/github" class="btn btn-secondary">Register With GitHub</a>
         </div>
     </div>
+
+    <script type="text/javascript">
+      function register() {
+        var data = {
+          'name': $('#register #name').val(),
+          'email': $('#register #email').val(),
+          'password': $('#register #password').val(),
+          'password_confirmation': $('#register #password_confirmation').val(),
+        }
+
+        $.ajax(
+          {
+            url: 'http://local.laravel55.com/api/user',
+            data: data,
+            type: 'POST',
+            dataType: 'json',
+          }
+        ).done(
+          function (json) {
+            $('#alert').addClass('alert-success').removeClass('hide').html('Successfully Registered')
+            $('#register input').val('')
+          }
+        ).fail(
+          function (xhr, status, errorThrown) {
+            $('#alert').addClass('alert-danger').removeClass('hide').html('Failed to Register')
+          }
+        )
+      }
+
+    </script>
 @endsection
